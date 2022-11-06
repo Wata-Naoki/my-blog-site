5474aa05f9cb423a7e450a39b04a419c4a2d20e3
Wata-Naoki <nao70blog@gmail.com>
2022-11-07 01:16:20 +0900

    detailpageのコメント一部の作成

--- components/Author.tsx
+++ components/Author.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import Image from "next/image";
 type Props = {
   author: any;
 };
 const Author = ({ author }: Props) => {
   return (
-    <div>
-      <h1>Author</h1>
+    <div className="relative p-12 mt-20 mb-8 text-center bg-black rounded-lg bg-opacity-20">
+      <div className="absolute left-0 right-0 -top-14">
+        <Image
+          unoptimized
+          alt={author.name}
+          height="100"
+          width="100"
+          className="object-center w-24 h-24 mx-auto rounded-full"
+          src={author.photo.url}
+        />
+      </div>
+      <h3 className="my-4 text-xl font-bold text-white">{author.name}</h3>
+      <p className="text-lg text-white">{author.bio}</p>
     </div>
   );
 };
--- components/CommentsForm.tsx
+++ components/CommentsForm.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const CommentsForm = ({ slug }: any) => {
+  const [error, setError] = useState(false);
+  const [message, setMessage] = useState(null);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const nameEl = useRef();
+  const emailEl = useRef();
+  //commentElのuseRefの型を定義する
+  const commentEl = useRef<HTMLTextAreaElement>(null);
+  const storeDataEl = useRef();
+
   return (
-    <div>
-      <h1>CommentsForm</h1>
+    <div className="p-8 pb-12 mb-8 bg-white shadow-lg">
+      <h3 className="pb-4 mb-8 text-xl font-semibold border-b">CommentsForm</h3>
+      <div className="grid grid-cols-1 gap-4 mb-4 ">
+        <textarea
+          ref={commentEl}
+          className="w-full p-4 text-gray-700 bg-gray-100 rounded-lg outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="comment"
+          name="comment"
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-4 mb-4 "></div>
+      <div className="grid grid-cols-1 gap-4 mb-4 "></div>
     </div>
   );
 };
